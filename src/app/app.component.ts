85684f755bb78e73a0fcb7d5eb39b4274a5a6d1a
1952754 <1952754@tcs.com>
2024-08-16 12:51:16 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+interface Order {
+  dish: string;
+  customers: string[];
+ }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'e-commerce-app';
+  orders: Order[] = [];
+ addOrder(customerNameInput: HTMLInputElement, dishSelect: HTMLSelectElement): void {
+   const customerName = customerNameInput.value.trim();
+   const selectedDish = dishSelect.value;
+   if (customerName) {
+     // Find if the dish already exists in the orders array
+     const existingOrder = this.orders.find(order => order.dish === selectedDish);
+     if (existingOrder) {
+       // If it exists, add the customer's name to the customers array
+       existingOrder.customers.push(customerName);
+     } else {
+       // If it doesn't exist, create a new order
+       this.orders.push({ dish: selectedDish, customers: [customerName] });
+     }
+     // Clear the input fields after adding the order
+     customerNameInput.value = '';
+     dishSelect.value = 'Burger'; // Set to default option
+   }
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarModule } from 'primeng/menubar';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    TabMenuModule,
+    RouterModule.forRoot([{ path: '', component: AppComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
